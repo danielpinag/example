9ae5a1a97a9e322fce615e359e2d2b05aee830df
danielpinag <danielalexpg@gmail.com>
2019-08-23 15:45:42 -0500

    added first month exercise

---
+++ ejercicio-primer-mes/assets/scripts/index.js
@@ -0,0 +1,60 @@
+var pageNumber = 1;
+function previousPage(){
+  pageNumber = pageNumber - 1;
+  $(".avatar-card").remove()
+  $(".change-page").remove()
+  loadPage()
+  window.history.pushState({}, "Avatares", `?page=${pageNumber}`);
+}
+function nextPage(){
+  pageNumber = pageNumber + 1;
+  $(".avatar-card").remove()
+  $(".change-page").remove()
+  loadPage()
+  window.history.pushState({}, "Avatares", `?page=${pageNumber}`);
+}
+
+function loadPage(){
+  var queryURL = `https://reqres.in/api/users?page=${pageNumber}`;
+  $.ajax({
+    url: queryURL,
+    method: "GET"
+  }).done(function(response) {
+    const departmentArrays = response.data;
+    var departmentBox = "";
+    const pagination = `${response.page} de ${response.total_pages}`
+    $(".page").empty().append(pagination);
+    var previousPageButton = "";
+    var nextPageButton = "";
+    if(pageNumber != 1){
+      previousPageButton = `<div class="change-page" onclick="previousPage()"><</div>`
+      $(".previous-page").empty().append(previousPageButton);
+    };
+    if(pageNumber != response.total_pages){
+      nextPageButton = `<div class="change-page" onclick="nextPage()">></div>`
+      $(".next-page").empty().append(nextPageButton);
+    };
+    departmentArrays.forEach(function(avatar) {
+      departmentBox = 
+      `<div class="avatar-card">
+         <div class="avatar-data">
+           <div class="card-left">
+             <img src="${avatar.avatar}" class="avatar-image">
+           </div>
+           <div class="card-right">
+             <div class="avatar-id">${avatar.id}</div>
+             <div class="avatar-name">${avatar.first_name} ${avatar.last_name}</div>
+             <div class="avatar-phone">+52 1 999 234 5678</div>
+           </div>
+           <div class="card-bottom">
+             <div class="avatar-email">${avatar.email}</div>
+           </div>
+         </div> 
+       </div>`;
+      $(".avatar-zone").append(departmentBox)
+    });
+  });
+};
+
+
+//window.history.pushState("object or string", "Page Title", "/newURL");
